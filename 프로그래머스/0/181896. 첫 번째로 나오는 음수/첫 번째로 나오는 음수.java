613d3a9f2b2a9bf5633674d72235e7fd1113a4c0
최홍서 <ghdtj07@gmail.com>
2026-03-30 19:50:36 +0900

    [level 0] Title: 첫 번째로 나오는 음수, Time: 0.03 ms, Memory: 73.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/181896. 첫 번째로 나오는 음수/첫 번째로 나오는 음수.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+import java.io.IOException;
+
+public class Solution {
+    public static int solution(int[] num_list) {
+        int count = num_list.length;
+        for(int i=0; i<count; i++){
+            if(num_list[i]<0){
+                return i;
+            }
+        }
+        return -1;
+    }
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(),", ");
+        int count = st.countTokens();
+        int[] arr = new int[count];
+        for(int i=0; i<count; i++){
+            arr[i]=Integer.parseInt(st.nextToken());
+        }
+        System.out.println(solution(arr));
+    }
+}
