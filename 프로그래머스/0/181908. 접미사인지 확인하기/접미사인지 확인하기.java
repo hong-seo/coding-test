f82384d0d7545094daaa4e4f1554f74818731ce5
최홍서 <ghdtj07@gmail.com>
2026-03-31 18:20:57 +0900

    [level 0] Title: 접미사인지 확인하기, Time: 0.04 ms, Memory: 84.4 MB -BaekjoonHub

--- 프로그래머스/0/181908. 접미사인지 확인하기/접미사인지 확인하기.java
+++ 프로그래머스/0/181908. 접미사인지 확인하기/접미사인지 확인하기.java
@@ -4,11 +4,19 @@ import java.io.IOException;
 
 public class Solution {
     public static int solution(String my_string, String is_suffix) {
-        if(my_string.endsWith(is_suffix)){
-            return 1;
+        int count1 = my_string.length();
+        int count2 = is_suffix.length();
+        if(count1<count2){
+            return 0;
         }
-        return 0;
+        for(int i=1; i<=count2; i++){
+            if(my_string.charAt(count1-i)!=is_suffix.charAt(count2-i)){
+            return 0;
+            }
+        }
+        return 1;
     }
+    
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String my = br.readLine();
