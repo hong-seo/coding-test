864a7c3a5490b36a9fa1ae3db643f5d7a9736c30
최홍서 <ghdtj07@gmail.com>
2026-03-31 18:10:02 +0900

    [level 0] Title: 특별한 이차원 배열 1, Time: 0.37 ms, Memory: 90.6 MB -BaekjoonHub

--- 프로그래머스/0/181833. 특별한 이차원 배열 1/특별한 이차원 배열 1.java
+++ 프로그래머스/0/181833. 특별한 이차원 배열 1/특별한 이차원 배열 1.java
@@ -1,26 +1,6 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.io.IOException;
-
-public class Solution {
-    public static int[][] solution(int n) {
-        int[][] arr = new int[n][n];
-        for(int i =0; i<n; i++){
-            for(int j=0; j<n; j++){
-                if(i==j){
-                    arr[i][j]=1;
-                }
-                else{
-                    arr[i][j] = 0;
-                }
-            }
-        }
-        return arr;
-    }
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-        System.out.println(Arrays.deepToString(solution(n)));
+class Solution {
+    public int[][] solution(int n) {
+        int[][] answer = {};
+        return answer;
     }
 }
