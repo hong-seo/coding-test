ed9679137f23c4577956f5d6dc1abe6bdb3bb29c
최홍서 <ghdtj07@gmail.com>
2026-04-27 15:30:37 +0900

    [level 0] Title: 가까운 1 찾기, Time: 0.03 ms, Memory: 82.9 MB -BaekjoonHub

---
+++ 프로그래머스/0/181898. 가까운 1 찾기/가까운 1 찾기.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+import java.io.IOException;
+
+public class Solution {
+    public static int solution(int[] arr, int idx) {
+        for(int i =idx; i<arr.length;i++){
+            if(arr[i]==1){
+                return i;
+            }
+        }
+        return -1;
+    }
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(),", ");
+        int count = st.countTokens();
+        int[] arr = new int[count];
+        for(int i=0; i<count; i++){
+            arr[i]=Integer.parseInt(st.nextToken());
+        }
+        int num = Integer.parseInt(br.readLine());
+        System.out.println(solution(arr, num));
+    }
+}
