bd1829335bcabf5c483de26b5e8c183dad2623b2
최홍서 <ghdtj07@gmail.com>
2026-05-15 20:18:11 +0900

    [level 0] Title: 문자열 출력하기, Time: 118.65 ms, Memory: 59.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/181952. 문자열 출력하기/문자열 출력하기.java
@@ -0,0 +1,13 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String a = sc.next();
+        System.out.println(a);
+    }
+}
