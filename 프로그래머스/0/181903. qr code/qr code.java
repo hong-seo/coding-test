ff2eb49a373d3f8a36ad8776fcf804280dd3f88f
최홍서 <ghdtj07@gmail.com>
2026-06-01 00:10:03 +0900

    [level 0] Title: qr code, Time: 0.05 ms, Memory: 63.1 MB -BaekjoonHub

--- 프로그래머스/0/181903. qr code/qr code.java
+++ 프로그래머스/0/181903. qr code/qr code.java
@@ -5,20 +5,17 @@ import java.io.IOException;
 public class Solution {
     public static String solution(int q, int r, String code) {
         StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < code.length(); i++) {
-            if (i % q == r) {
-                sb.append(code.charAt(i));
-            }
+        for(int i=r; i<code.length(); i+=q){
+            sb.append(code.charAt(i));
         }
         return sb.toString();
     }
-    
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int q = Integer.parseInt(br.readLine());
         int r = Integer.parseInt(br.readLine());
         String code = br.readLine();
-        code = code.replace("\"", "");
+        code = code.replace("\"","");
         System.out.println(solution(q, r, code));
     }
 }
