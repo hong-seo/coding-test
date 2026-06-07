073b35531d6cfb110fce373a7d91d2f5bbb51c77
최홍서 <ghdtj07@gmail.com>
2026-06-08 00:12:15 +0900

    [level 0] Title: 수열과 구간 쿼리 2, Time: 1.63 ms, Memory: 67.5 MB -BaekjoonHub

--- 프로그래머스/0/181923. 수열과 구간 쿼리 2/수열과 구간 쿼리 2.java
+++ 프로그래머스/0/181923. 수열과 구간 쿼리 2/수열과 구간 쿼리 2.java
@@ -7,45 +7,45 @@ import java.io.IOException;
 public class Solution {
     public static int[] solution(int[] arr, int[][] queries) {
         int[] answer = new int[queries.length];
-        for (int i = 0; i < queries.length; i++) {
+        for(int i=0; i<queries.length; i++){
             int s = queries[i][0];
             int e = queries[i][1];
             int k = queries[i][2];
-            int minVal = Integer.MAX_VALUE;
-            for (int j = s; j <= e; j++) {
-                if (arr[j] > k && arr[j] < minVal) {
-                    minVal = arr[j];
+            int min=-1;
+            for(int j=s; j<=e; j++){
+                if(arr[j]>k){
+                    if(min==-1){
+                        min =arr[j];
+                    }
+                    if(min>arr[j]){
+                        min = arr[j];
+                    }
                 }
             }
-            if (minVal == Integer.MAX_VALUE) {
-                answer[i] = -1;
-            } else {
-                answer[i] = minVal;
-            }
+            answer[i]=min;
         }
         return answer;
     }
-    
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String ar = br.readLine();
-        ar = ar.substring(1, ar.length() - 1);
-        StringTokenizer st = new StringTokenizer(ar, ", ");
+        ar = ar.substring(1, ar.length()-1);
+        StringTokenizer st = new StringTokenizer(ar,", ");
         int count = st.countTokens();
         int[] arr = new int[count];
-        for (int i = 0; i < count; i++) {
+        for(int i=0; i<count; i++){
             arr[i] = Integer.parseInt(st.nextToken());
         }
         String que = br.readLine();
-        que = que.substring(2, que.length() - 2);
+        que = que.substring(2, que.length()-2);
         String[] query = que.split("\\],\\[");
-        int[][] queries = new int[query.length][];        
-        for (int i = 0; i < query.length; i++) {
-            st = new StringTokenizer(query[i], ", ");
+        int[][] queries=new int[query.length][];
+        for(int i=0; i<query.length; i++){
+            st = new StringTokenizer(query[i],", ");
             count = st.countTokens();
-            queries[i] = new int[count];            
-            for (int j = 0; j < count; j++) {
-                queries[i][j] = Integer.parseInt(st.nextToken());
+            queries[i] = new int[count];
+            for(int j=0; j<count; j++){
+                queries[i][j]= Integer.parseInt(st.nextToken());
             }
         }
         System.out.println(Arrays.toString(solution(arr, queries)));
